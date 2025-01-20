5bd3d251db5f4dbc83ba6cf5ef43c368c8dd3072
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-01-20 15:56:54 +0530

    Profit loss.java

---
+++ Profit loss.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    float a=60;
+	    float b=4;
+	    float c=b*12;
+	    float d=a-c;
+	    if(c>a)
+	    {
+	        System.out.printf("%.2f  profit",d);
+	    }
+	    else{
+	        System.out.printf("%.2f  loss",d);
+	        
+	    }
+	    
+	    
+	  
+	} 
+}
