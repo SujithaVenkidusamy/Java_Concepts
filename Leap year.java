5b73e3855b10403295ab1a413e185e8ecd778896
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-01-20 15:05:03 +0530

    Leap year.java

---
+++ Leap year.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=2000;
+	    if(a % 4 ==0||(a%400 ==0 && a%100!=0))
+	    {
+	      System.out.println("leap");
+	    }
+	    else {
+	        System.out.println(" not leap");
+	        
+	    }
+	  
+	} 
+}
