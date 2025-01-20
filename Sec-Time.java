97cbf15697dcaebffc243a9534de7189b2d6a1d7
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-01-20 17:20:30 +0530

    Sec-Time.java

--- Sec-Time.java
+++ Sec-Time.java
@@ -14,3 +14,23 @@ public class Main
 	   System.out.println("Sec:"+sec);
 	} 
 }
+
+****************************************************
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner s=new Scanner(System.in);
+	   System.out.println("enter Seconds:");
+	   long d=s.nextInt();
+	   long days=d/(24*3600);
+	   long hour=d/3600;
+	   long remain=d%3600;
+	   long min=remain/60;
+	   long sec=remain%60;
+	   System.out.println("Hour:"+hour);
+	   System.out.println("Min:"+min);
+	   System.out.println("Sec:"+sec);
+	} 
+}
+
