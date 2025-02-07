5f67d0b88799b866b21b7e216a58cd9250c56fba
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 22:01:59 +0530

    Class with attribute.java

---
+++ Class & Objects/Class with attribute.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    String name;
+    int age;
+    
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    String name=s.nextLine();
+	    int age=s.nextInt();
+		System.out.println("Name is:"+name);
+		System.out.println("Age is:"+age);
+	}
+}
