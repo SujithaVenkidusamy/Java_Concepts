bf7dad5836678944b8b03a7a53df7970b03fc52a
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:30:41 +0530

    CharAt.java

---
+++ Strings/CharAt.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="Hello World";
+	    char ch=str.charAt(4);// 4th index value
+		System.out.println("Statement :" +ch);
+	}
+}
