f6970e64da1f56f9266b4b93461a50db531e74fa
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:45:42 +0530

    Check if a string is having only alphabetical characters.java

---
+++ Strings/Check if a string is having only alphabetical characters.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="Welcome to JAVA training";
+	    if(str.matches("[a-z A-Z]+")){
+	        System.out.println("Alphabetical characters ="+true);
+	        
+	    }
+	    else{
+	         System.out.println("Alphabetical characters ="+false);
+	        
+	    }
+	   
+	}
+}
