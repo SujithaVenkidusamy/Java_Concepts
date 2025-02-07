07f53d0061292ad2ebbc584b00a89590f93ff32b
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:35:48 +0530

    Last indexOf.java

---
+++ Strings/Last indexOf.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="HELLO WORLD";
+		System.out.println("LastIndexOf :" +str.lastIndexOf('L'));//l-small,I-caps(Camel case)
+	}
+}
