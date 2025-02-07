3ba91302925504e35a1d69908a4c7fcf1097a813
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 20:55:56 +0530

    Left Arrow Triangle.java

---
+++ Patterns/Left Arrow Triangle.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    System.out.print("Enter n:");
+	    int n=s.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int sp=1;sp<=n-i;sp++){
+	            System.out.print("  ");
+	        }
+	        
+	        for(int j=1;j<=i;j++){
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	    for(int i=n-1;i>=1;i--){
+	        for(int sp=1;sp<=n-i;sp++){
+	            System.out.print("  ");
+	        }
+	        
+	        for(int j=1;j<=i;j++){
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	    
+   }
+
+}
