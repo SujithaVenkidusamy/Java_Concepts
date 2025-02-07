de8e2133d10834905371b67a7e3e938a0284d482
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:41:32 +0530

    Trim.java

---
+++ Strings/Trim.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="  Welcome to JAVA training  ";
+	    System.out.println("Trim : " +str.trim());//removes extra spaces at starting and ending 
+	}
+}
