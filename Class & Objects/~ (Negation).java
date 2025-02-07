4e2ade7e2ec5a69582ccacc2f358ce3519fe069b
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 22:04:44 +0530

    ~ (Negation).java

---
+++ Class & Objects/~ (Negation).java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int num=s.nextInt();
+	    System.out.print(~num);
+
+	}
+}
