fffc9f2a1d6b11a8420a7dd1b8c122969190bb92
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:17:37 +0530

    Sort the array(Built-in).java

---
+++ Arrays/Sort the array(Built-in).java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    System.out.print("Enter size of array :");
+	    int n=s.nextInt();
+	    int arr[]=new int[n];
+	    
+	    System.out.print("Enter elements :");
+	    for(int i=0;i<n;i++){
+	        arr[i]=s.nextInt();
+	    }
+	    
+	    Arrays.sort(arr);
+	     System.out.print("Ascending order :");
+	     for(int i=0;i<arr.length;i++){
+	         System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+	        
+}	    
+}
