90efee3bdef087ae7b0bee5540c091f4d2a2f72a
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-07 21:42:31 +0530

    Replace.java

---
+++ Strings/Replace.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="  Welcome to JAVA training  ";
+	    System.out.println("Replace : " +str.replace('t','T')); 
+	}
+}
