0ce3134803121903bba62126e52d1484c003920b
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-08 09:33:12 +0530

    Selection Sort.java

--- Sorting/Selection Sort.java
+++ Sorting/Selection Sort.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {  
    public static void main(String args[]) {  
         int arr[]={4,6,1,9,5,3};
-     int n = arr.length;
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
          
             int min = i;
@@ -16,8 +16,8 @@ public class Main {
 
          
            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;  
+           arr[i] = arr[min];
+           arr[min] = temp;  
            
         }
           for (int val : arr) {
