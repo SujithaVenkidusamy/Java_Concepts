5a11265dd5a0215dc22535fd160c7821181866cc
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-09 09:46:14 +0530

    Even & Odd numbers.java

---
+++ Looping/Even & Odd numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s =new Scanner(System.in);
+	    int a =s.nextInt();
+		for(int i=1;i<a;i++)
+		{
+		    if(i%2==0){
+		        System.out.print(i +" ");
+		    }
+		}
+		System.out.println();
+		for(int i=1;i<a;i++)
+		{
+		    if(i%2!=0){
+		        System.out.print(i + " ");
+		    }
+		}
+	}
+}
