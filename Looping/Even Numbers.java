4ebfc5fb5339127beefa3792ada6162781ec58c1
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-09 09:44:18 +0530

    Even Numbers.java

---
+++ Looping/Even Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s =new Scanner(System.in);
+	    int a =s.nextInt();
+		for(int i=1;i<a;i++)
+		{
+		    if(i%2==0){
+		        System.out.print(i +" ");
+		    }
+		}
+		
+	}
+}
