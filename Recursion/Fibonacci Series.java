e6b41cbb83944573578f8ac9606ec7388359fb3e
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-10 10:34:44 +0530

    Fibonacci Series.java

--- Recursion/Fibonacci Series.java
+++ Recursion/Fibonacci Series.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+public class Main
+{
+    public static int fibonacci(int n){
+        if(n<=1){ //Base case
+            return n;
+        }
+        else{
+            return fibonacci(n-1) + fibonacci(n-2) ; //Recursive case
+        }
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int number=sc.nextInt();
+	    for(int i=0;i<number;i++){
+		System.out.println("The fibonacci is: "+fibonacci(i));
+	    }
+	}
+}
