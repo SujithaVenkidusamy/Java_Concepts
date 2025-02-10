c079cdc352f8c510ebf38ba7557fe0fd3f8ae8a8
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-10 10:22:48 +0530

    Factorial.java

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Main
 {
     public static int factorial(int n){
-        if(n==0 || n<0){
+        if(n==0 || n<0){ //Base case
             return 1;
         }
         else{
-            return n*factorial(n-1);
+            return n*factorial(n-1); //Recursive case
         }
     }
 	public static void main(String[] args) {
