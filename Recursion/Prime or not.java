d78b93d3064a2b14c655fce24821d87832dd7e6c
Sujitha Venkidusamy <162794959+SujithaVenkidusamy@users.noreply.github.com>
2025-02-10 11:27:20 +0530

    Update Prime or not.java

--- Recursion/Prime or not.java
+++ Recursion/Prime or not.java
@@ -1,25 +1,28 @@
 import java.util.*;
-public class Main
-{
-    public static boolean prime(int n,int i){
-        if(n<=1){ //Base case
+
+public class Main {
+    
+    // checks if a number 'n' is prime, using recursion with an iterator 'i'
+    public static boolean prime(int n, int i) {
+        if (n <= 1) { // Base case: numbers less than or equal to 1 are not prime
             return false;
         }
-        if(i==1){
+        if (i == 1) { // Base case: when the iterator reaches 1, the number is prime
             return true;
-        } 
-        if(n%i==0){
+        }
+        if (n % i == 0) { // If the number is divisible by 'i', it's not prime
             return false;
-        } 
-        return prime(n,i-1);
-       
+        }
+        return prime(n, i - 1); // Recursive call with 'i' reduced by 1
     }
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    int number=sc.nextInt();
-	    boolean result=prime(number,number/2);
-	    if(result) System.out.print("Prime number");
-		else System.out.print("Not a Prime number");
-	    
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int number = sc.nextInt(); 
+        boolean result = prime(number, number / 2); // Call the 'prime' method, starting with half of the number as the iterator
+        if (result) 
+            System.out.print("Prime number");
+        else 
+            System.out.print("Not a Prime number");
+    }
 }
